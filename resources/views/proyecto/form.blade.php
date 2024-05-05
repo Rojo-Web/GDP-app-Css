a2a596fd04526743165ff00d470187f4b4dc667c
Rojo-Web <santifa2004@gmail.com>
2024-05-05 11:44:18 -0500

    ultimos cambios del formulario de crear proyecto

--- resources/views/proyecto/form.blade.php
+++ resources/views/proyecto/form.blade.php
@@ -1,52 +1,4 @@
-<!-- <div class="row padding-1 p-1">
-    <div class="col-md-12">
 
-        <div class="form-group mb-2 mb20">
-            <label for="nombre" class="form-label">{{ __('Nombre') }}</label>
-            <input type="text" name="nombre" class="form-control @error('nombre') is-invalid @enderror" value="{{ old('nombre', $proyecto?->nombre) }}" id="nombre" placeholder="Nombre">
-            {!! $errors->first('nombre', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="estado" class="form-label">{{ __('Estado') }}</label>
-            <input type="text" name="estado" class="form-control @error('estado') is-invalid @enderror" value="{{ old('estado', $proyecto?->estado) }}" id="estado" placeholder="Estado">
-            {!! $errors->first('estado', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="descripcion" class="form-label">{{ __('Descripcion') }}</label>
-            <input type="text" name="descripcion" class="form-control @error('descripcion') is-invalid @enderror" value="{{ old('descripcion', $proyecto?->descripcion) }}" id="descripcion" placeholder="Descripcion">
-            {!! $errors->first('descripcion', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="fecha_inicio" class="form-label">{{ __('Fecha Inicio') }}</label>
-            <input type="text" name="fecha_inicio" class="form-control @error('fecha_inicio') is-invalid @enderror" value="{{ old('fecha_inicio', $proyecto?->fecha_inicio) }}" id="fecha_inicio" placeholder="Fecha Inicio">
-            {!! $errors->first('fecha_inicio', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="fecha_final" class="form-label">{{ __('Fecha Final') }}</label>
-            <input type="text" name="fecha_final" class="form-control @error('fecha_final') is-invalid @enderror" value="{{ old('fecha_final', $proyecto?->fecha_final) }}" id="fecha_final" placeholder="Fecha Final">
-            {!! $errors->first('fecha_final', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="lider_id" class="form-label">{{ __('Lider Id') }}</label>
-            <input type="text" name="lider_id" class="form-control @error('lider_id') is-invalid @enderror" value="{{ old('lider_id', $proyecto?->lider_id) }}" id="lider_id" placeholder="Lider Id">
-            {!! $errors->first('lider_id', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="integrantes" class="form-label">{{ __('Integrantes') }}</label>
-            <label for="integrantes">Selecciona integrantes:</label>
-            <select id="integrantes" name="integrantes[]" multiple>
-                @foreach ($users as $user)
-                <option value="{{$user->cedula}}"> {{$user->name}}</option>
-                @endforeach
-            </select>
-        </div>
-
-
-    </div>
-    <div class="col-md-12 mt20 mt-2">
-        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
-    </div>
-</div> -->
 
 
 <div class="row" style="padding: 0% 5% 5% 5%;justify-content: center;">
@@ -106,7 +58,7 @@
             {!! $errors->first('fecha_final', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         <div class="form-group mb-2 mb20">
-            <label for="integrantes">Selecciona los integrantes con [ctrol]:</label>
+            <label for="integrantes">Selecciona los integrantes con [control]:</label>
             <select id="integrantes" class="form-select" name="integrantes[]" multiple>
                 @foreach ($users as $user)
                 <option value="{{$user->cedula}}"> {{$user->name}}</option>
